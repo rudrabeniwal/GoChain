152deed218111580c54b7210e28ecb9637e6a90a
Rudra Beniwal <rudrabeniwal@icloud.com>
2024-05-14 01:13:03 +0530

    Made different files for different functions and workings

---
+++ Phase - I/Proof-Of-Work/blockchain.go
@@ -0,0 +1,15 @@
+package main
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
